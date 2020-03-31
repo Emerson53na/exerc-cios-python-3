9112a39f093468202535314075a2c9556038d172
Emerson Sena <62801061+Emerson53na@users.noreply.github.com>
2020-03-31 12:21:19 -0300

    Exercício 005

---
+++ 005 Antecessor e Sucessor.py
@@ -0,0 +1,2 @@
+n = int(input('digite um número:\033[33m '))
+print('\033[mAnalisando o valor \033[33m{}\033[m...\nSeu antecessor é \033[32m{}\033[m e seu sucesso é \033[32m{}\033[m.'.format(n,(n-1),(n+1)))
