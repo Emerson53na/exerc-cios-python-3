1e6d7292079d2acc6f34cd9cc87411a915f6a65b
Emerson Sena <62801061+Emerson53na@users.noreply.github.com>
2020-04-01 12:12:42 -0300

    Exercício 012

--- 012 Calculando desconto.py
+++ 012 Calculando desconto.py
@@ -1,3 +1,3 @@
 n = float(input('Qual é o preço do produto? R$'))
-d = n*5/100
+d = n * 5 / 100
 print('O produto que custava R${:.2f} na promoção com desconto de 5% vai custar \033[32mR${:.2f}\033[m.'.format(n,(n-d)))
