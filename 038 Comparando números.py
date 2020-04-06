efdf63d96045d3c65cfcd505e59d7a3c172b01bf
Emerson Sena <62801061+Emerson53na@users.noreply.github.com>
2020-04-06 06:38:52 -0300

    Exercício 038

---
+++ 038 Comparando números.py
@@ -0,0 +1,19 @@
+import os
+
+while True:
+      os.system('cl' if os.name == 'nl' else 'clear')
+ 
+      num1 = int(input(' Primeiro número: '))
+      num2 = int(input(' Segundo número: '))
+ 
+      if num1 > num2:
+            print('\n O PRIMEIRO número é maior.')
+            input(' \033[32mEnter... \033[m')
+  
+      elif num1 < num2:
+            print('\n O SEGUNDO número é maior.')
+            input(' \033[32mEnter... \033[m')
+  
+      else:
+            print('\n Os dois valores são IGUAIS.')
+            input(' \033[32mEnter... \033[m')
